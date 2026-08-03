7fe7378a1b0a9efaa0e57f4acc50594a782751c4
wwwnoornoor1noornoor-dotcom <www.noornoor1noornoor@gmail.com>
2026-08-04 02:44:28 +0300

    Update flip.js

--- flip.js
+++ flip.js
@@ -1,4 +1,4 @@
-
+/*
 pdfjsLib.GlobalWorkerOptions.workerSrc =
 "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
 
@@ -11,5 +11,25 @@ convertBtn.addEventListener("click", loadPDF);
 async function loadPDF(){
 
     alert("بدأ التحويل");
+
+}
+*/
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
+
+const fileInput = document.getElementById("pdfFile");
+const convertBtn = document.getElementById("convertBtn");
+const book = document.getElementById("book");
+
+convertBtn.addEventListener("click", loadPDF);
+
+async function loadPDF(){
+
+    if(typeof St === "undefined"){
+        alert("مكتبة PageFlip لم يتم تحميلها");
+        return;
+    }
+
+    alert("مكتبة PageFlip جاهزة ✅");
 
 }
