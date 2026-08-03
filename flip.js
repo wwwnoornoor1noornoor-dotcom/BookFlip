475330cbbd65c9581198645c32c60aaabceccada
wwwnoornoor1noornoor-dotcom <www.noornoor1noornoor@gmail.com>
2026-08-04 02:51:28 +0300

    Update flip.js

--- flip.js
+++ flip.js
@@ -22,13 +22,38 @@ const convertBtn = document.getElementById("convertBtn");
 const book = document.getElementById("book");
 
 convertBtn.addEventListener("click", loadPDF);
-
 async function loadPDF(){
+
+    const flipBook = new St.PageFlip(
+        document.getElementById("book"),
+        {
+            width:450,
+            height:600
+        }
+    );
+
+    const pages = [];
+
+    for(let i=1;i<=6;i++){
+
+        const div = document.createElement("div");
+
+        div.innerHTML =
+        "<h2 style='text-align:center;margin-top:250px;'>الصفحة "+i+"</h2>";
+
+        pages.push(div);
+
+    }
+
+    flipBook.loadFromHTML(pages);
+
+}
+/*async function loadPDF(){
 
     if(typeof St === "undefined"){
         alert("مكتبة PageFlip لم يتم تحميلها");
         return;
-    }
+    }*/
 
     alert("مكتبة PageFlip جاهزة ✅");
 
