02c2762323f5b6224231a813dd2294c79b14dc86
wwwnoornoor1noornoor-dotcom <www.noornoor1noornoor@gmail.com>
2026-08-04 02:05:00 +0300

    Update flip.js

--- flip.js
+++ flip.js
@@ -1,1 +1,15 @@
 
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+"https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js";
+
+const fileInput = document.getElementById("pdfFile");
+const convertBtn = document.getElementById("convertBtn");
+const book = document.getElementById("book");
+
+convertBtn.addEventListener("click", loadPDF);
+
+async function loadPDF(){
+
+    alert("بدأ التحويل");
+
+}
